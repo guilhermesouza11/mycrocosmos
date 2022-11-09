35476b44f089d411bc531c5d59e6b4863c9a2a94
guilhermesouza11 <guilhermesouzadossantos11@gmail.com>
2022-11-09 17:24:55 -0300

    fazendo script de medidas

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 window.addEventListener("scroll", function(){
     var header = this.document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-})
+});
+
+window.addEventListener("scroll", function(){
+    var img = this.document.querySelector("img");
+    img.classList.toggle("decrease", window.scrollY > 0);
+});
+
+var buy = document.querySelector("#buy");
+var actions = document.querySelector(".actionsCard");
+
+buy.addEventListener("click", function(){
+    if(actionsCard.style.display === 'none'){
+        actionsCard.style.display === 'flex';
+    }else{
+        actionsCard.style.display === 'none';
+    }
+});
